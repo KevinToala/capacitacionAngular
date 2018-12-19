c1c1fc6245a877aa20268a0a6938f9b58ab17585
Kevin Toala <kevin.toala@outlook.com>
2018-12-19 14:43:02 -0500

    Corrección facturas para taller directivas

--- dia3/TallerDirectivas/facturas.ts
+++ dia3/TallerDirectivas/facturas.ts
@@ -1,77 +1,77 @@
-  public facturas: Factura[] = [
-    {
-      numeroFactura: '001-001-000000001',
-      cliente: 'Kevin Toala Mosquera',
-      anulado: false,
-      items: [
-        {
-          producto: 'Desarrollo de software',
-          cantidad: 1,
-          precioUnitario: 400
-        }
-      ]
-    },
-    {
-      numeroFactura: '001-001-000000002',
-      cliente: 'Rosalinda Estrada',
-      anulado: false,
-      items: [
-        {
-          producto: 'Libro de economia',
-          cantidad: 1,
-          precioUnitario: 29.99
-        }
-      ]
-    },
-    {
-      numeroFactura: '001-001-000000003',
-      cliente: 'David Toala Mosquera',
-      anulado: true,
-      items: [
-        {
-          producto: 'Discos para gimnasio',
-          cantidad: 10,
-          precioUnitario: 29.99
-        },
-        {
-          producto: 'Barras para gimnasio',
-          cantidad: 10,
-          precioUnitario: 10
-        }
-      ]
-    },
-    {
-      numeroFactura: '001-001-000000004',
-      cliente: 'Solange Jarammillo',
-      anulado: false,
-      items: [
-        {
-          producto: 'Leche deslactosada',
-          cantidad: 4,
-          precioUnitario: 2.99
-        },
-        {
-          producto: 'Manzana',
-          cantidad: 2,
-          precioUnitario: 1
-        }
-        {
-          producto: 'Cereales',
-          cantidad: 1,
-          precioUnitario: 3.50
-        }
-      ]
-    },
-    {
-      numeroFactura: '001-001-000000005',
-      cliente: 'Kevin Toala Mosquera',
-      anulado: true,
-      items: [
-        {
-          producto: 'Norton Antivirus',
-          cantidad: 1,
-          precioUnitario: 600
-        }
-      ]
-    }
-  ];
+public facturas: Factura[] = [
+  {
+    numeroFactura: '001-001-000000001',
+    cliente: 'Kevin Toala Mosquera',
+    anulado: false,
+    items: [
+      {
+        producto: 'Desarrollo de software',
+        cantidad: 1,
+        precioUnitario: 400
+      }
+    ]
+  },
+  {
+    numeroFactura: '001-001-000000002',
+    cliente: 'Rosalinda Estrada',
+    anulado: false,
+    items: [
+      {
+        producto: 'Libro de economia',
+        cantidad: 1,
+        precioUnitario: 29.99
+      }
+    ]
+  },
+  {
+    numeroFactura: '001-001-000000003',
+    cliente: 'David Toala Mosquera',
+    anulado: true,
+    items: [
+      {
+        producto: 'Discos para gimnasio',
+        cantidad: 10,
+        precioUnitario: 29.99
+      },
+      {
+        producto: 'Barras para gimnasio',
+        cantidad: 10,
+        precioUnitario: 10
+      }
+    ]
+  },
+  {
+    numeroFactura: '001-001-000000004',
+    cliente: 'Solange Jarammillo',
+    anulado: false,
+    items: [
+      {
+        producto: 'Leche deslactosada',
+        cantidad: 4,
+        precioUnitario: 2.99
+      },
+      {
+        producto: 'Manzana',
+        cantidad: 2,
+        precioUnitario: 1
+      },
+      {
+        producto: 'Cereales',
+        cantidad: 1,
+        precioUnitario: 3.50
+      }
+    ]
+  },
+  {
+    numeroFactura: '001-001-000000005',
+    cliente: 'Kevin Toala Mosquera',
+    anulado: true,
+    items: [
+      {
+        producto: 'Norton Antivirus',
+        cantidad: 1,
+        precioUnitario: 600
+      }
+    ]
+  }
+];
